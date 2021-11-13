2b2f0f6f6685b27be63cf458db6842c86a1a71c8
c17an <kimchanmin1@gmail.com>
2021-11-13 16:33:22 +0900

    feat: scaffold, image tutorial

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(child: Text('Hello, Flutter!')),
-    ));
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('I am Rich!'),
+            backgroundColor: Colors.blueGrey,
+          ),
+          backgroundColor: Colors.blueGrey,
+          body: const Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://news.mit.edu/sites/default/files/images/202010/MIT-Metallic-Diamond-01-Press.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
