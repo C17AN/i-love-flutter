f81683c8a6494970c4557572b168e4e1a65af006
c17an <kimchanmin1@gmail.com>
2021-11-19 00:33:20 +0900

    chore: ui preview 설정

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -5,10 +5,10 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[50],
+        backgroundColor: Colors.blue[700],
         appBar: AppBar(
           title: Text('주사위 게임'),
-          backgroundColor: Colors.blue[200],
+          backgroundColor: Colors.blue[600],
         ),
         body: DicePage(),
       ),
@@ -19,6 +19,31 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: <Widget>[
+            // Image.asset('images/dice1.png'),
+            Expanded(
+              // Image.asset은 내부적으로 AssetImage ImageProvider를 사용하는 방식을 간결하게 지원하는 위젯이다.
+              child: Image.asset("images/dice2.png"),
+              // child: Image(
+              //   image: AssetImage('images/dice2.png'),
+              // ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 20,
+            ),
+            Expanded(
+              child: Image(
+                image: AssetImage('images/dice2.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
