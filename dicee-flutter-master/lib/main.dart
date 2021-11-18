8c14fc7425056535b751e2fb17075f79e2cb5fca
c17an <kimchanmin1@gmail.com>
2021-11-18 22:42:21 +0900

    feat: create dice game project

---
+++ dicee-flutter-master/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[50],
+        appBar: AppBar(
+          title: Text('주사위 게임'),
+          backgroundColor: Colors.blue[200],
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
